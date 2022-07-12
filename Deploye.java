f427caa77c146ead4b2d92cede7845962efceabd
prashant1996raj <prashant1996raj@gmail.com>
2022-07-12 10:55:10 +0530

    Added basic

---
+++ Deploye.java
@@ -0,0 +1,28 @@
+public class deploye
+ {
+	public static void main(String args[])
+
+	{
+         int a =20;
+	 int b=10;
+		System.out.println(" The No are " + a + " " + b);
+		add(a,b);
+		sub(a,b);
+		
+		
+       }
+		public static void add(int a ,int b)
+		
+		{
+			System.out.println("SUM IS : " + (a+b));
+		
+		}
+		
+		public static  void sub(int a,int b)
+		{
+			
+			System.out.println("SUB is  : " + (a-b));
+
+		}
+	
+}
